73c43cdeba7831634fb5ebe3c0c6a29265078824
Alex-0234 <alex.liska04@seznam.cz>
2026-08-14 03:23:09 +0200

    fix: fix the blog

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,15 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-// import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import staticAssetsIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache";
 
+/**
+ * Bez nastavené incrementalCache spadne OpenNext na výchozí S3 cache z AWS.
+ * Na Cloudflare žádné S3 není, takže čtení vždycky selže a předrenderovaná
+ * stránka se místo servírování zkusí vyrenderovat znovu ve workeru - a tam
+ * padne na tom, že /blog čte články ze souborů (500), případně skončí na 404.
+ *
+ * Tahle cache čte předrenderované stránky rovnou z Workers Assets. Přesně
+ * sedí na blog, který se celý generuje při buildu a nic se v něm nereviduje.
+ */
 export default defineCloudflareConfig({
-	// For best results consider enabling R2 caching
-	// See https://opennext.js.org/cloudflare/caching for more details
-	// incrementalCache: r2IncrementalCache
+	incrementalCache: staticAssetsIncrementalCache,
 });
